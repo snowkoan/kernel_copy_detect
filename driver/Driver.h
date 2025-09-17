b12a9beb3d2f38f9d72449c32f26aa65aafd6946
aallidin <aallidin@cisco.com>
2025-09-17 14:44:51 -0400

    Fix naming.

--- driver/Driver.h
+++ driver/Driver.h
@@ -2,7 +2,7 @@
 
 extern PFLT_FILTER g_Filter;
 
-#define DRIVER_PREFIX "Backup: "
+#define DRIVER_PREFIX "CopyDetect: "
 
 NTSTATUS InitMiniFilter(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath);
 NTSTATUS MinifilterUnload(FLT_FILTER_UNLOAD_FLAGS Flags);
